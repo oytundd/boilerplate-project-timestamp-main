8246cd6ea73552c9ac30ad098f3bfa19398037f2
oytundd <oytundd@gmail.com>
2024-02-03 14:49:04 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,47 @@
+// index.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+require('dotenv').config();
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+app.get('/api/:date',(req,res) => {
+  const dateParam = req.params.date;
+const unixTimesampRE = /^\d{13}$/g
+  if( !isNaN( new Date(dateParam) ) ){
+    console.log(`This date is valid: ${dateParam}`);
+    const newDate = new Date(dateParam);
+    res.json({ unix: Math.floor(newDate.getTime() ), utc:newDate })
+  }else if(dateParam.match(unixTimesampRE)){
+    console.log(`Unix timestamp detected: ${dateParam}`);
+    const newDate = new Date(dateParam * 1000);
+    res.json({unix: dateParam, utc: newDate});
+  }else{
+    console.log(`Invalid date detected: ${dateParam}`);
+    res.json({error:'Invalid Date'});
+  }
+});
+
+// your first API endpoint... 
+app.get("/api/hello", function (req, res) {
+  res.json({greeting: 'hello API'});
+});
+
+
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
